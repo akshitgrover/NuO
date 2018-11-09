765eb070daebc19a1a49a4e9b2e8acdc5a1f8cc8
Akshit Grover <akshit.grover2016@gmail.com>
2018-11-09 16:25:06 +0530

    DataInterface: Add inaterface to handle json file backstore

---
+++ dataInterface/interface.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+import siteData as sd
+
+def placeData():
+
+    currentDir = os.getcwd()
+
+    for key in sd.DATAFILESOBJECT.keys():
+        value = sd.DATAFILESOBJECT[key]
+        value = os.path.abspath(value)
+
+        dataFile = open(value, "r")
+        data = dataFile.read()
+        parsedJsonData = json.loads(data)
+
+        sd.DATAOBJECT[key] = parsedJsonData
