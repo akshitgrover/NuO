d070c5c4b09789350c67f4c19e93330b5203c738
Akshit Grover <akshit.grover2016@gmail.com>
2018-11-18 14:52:09 +0530

    Call nuoParser

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #! /usr/bin/python
 
 from configParser import parser
+import nuoParser as nps
 import siteData as sd
 
 if __name__ == "__main__":
     print("Starting to parse...", end = "\n\n")
     parser.parse("./config.yml")
     sd.siteData()
+    nps.parse()
