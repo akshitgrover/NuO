8f9895bc03d5f626af8a5b76dc3a7ef92d1dfa38
Akshit Grover <akshit.grover2016@gmail.com>
2018-11-18 20:29:37 +0530

    nuoParser: Move chainedPropertyAccess function to utils module

--- nuoParser/handlers/var.py
+++ nuoParser/handlers/var.py
@@ -2,26 +2,7 @@ import os
 
 import siteData as sd
 from .. import patterns as p
-
-#Function to access properties of object
-def chainedPropertyAccess(obj, arr = []):
-
-    #Check for unit case
-    if(len(arr) == 1):
-        try:
-            #Unit case, Return Value of chained properties of an object
-            return obj[arr[0]]
-        except KeyError:
-            #Return empty string when interrupt is a KeyError
-            return ""
-    else:
-        #Handle KeyError
-        try:
-            #Recursive to call access property of property of an object
-            return chainedPropertyAccess(obj[arr[0]], arr[1:])
-        except KeyError:
-            #Return empty string when interrupt is a KeyError
-            return ""
+from .utils import chainedPropertyAccess
 
 #Function to parse variables in a string expression
 def exp(expression):
