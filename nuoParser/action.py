876ddb06bf7e16ff42e4184f693f88d9376d9e70
Akshit Grover <akshit.grover2016@gmail.com>
2018-11-20 00:55:50 +0530

    nuoParser::action: Add actions for nuoParser

---
+++ nuoParser/action.py
@@ -0,0 +1,58 @@
+import os
+import shutil
+
+_openedFile = None
+
+ACTION = "file"
+
+HTML_DIRECTORY = os.path.abspath("build")
+
+"""Set to hold actions defined in nuoParser
+Description of actions
+
+"file": To write a line to build file"""
+actionSet = {"file"}
+
+#Function ot set current action for nuoParser
+def setAction(action):
+
+    #Refer to ACTION variable outside function scope
+    global ACTION
+
+    #Check if action is present is actionSet
+    if(action not in actionSet):
+        raise Exception("Action '{}' is not defined".format(action))
+
+    #Set ACTION to specified action
+    ACTION = action
+
+#Function to open output html file in "build/" folder by default
+def openFile(file):
+
+    #Refer to _openedFile variable outside function scope
+    global _openedFile
+    _openedFile = open(file, "+a")
+
+#Function to close output html file referd by _openedFile global variable
+def closeFile():
+
+    #Refer to _openedFile varaible outside function scope
+    global _openedFile
+    _openedFile.close() #Close file
+    _openedFile = None #Switch to None object
+
+#Function to execute current action for the nuoParser
+def takeAction(line, *args):
+
+    #Check if current action is file 
+    if(ACTION == "file"):
+
+        #Refer to _openedFile outside function scope
+        global _openedFile
+        
+        #Check if a file is opened
+        if(_openedFile is None):
+            raise Exception("HTML Output: No file is opened to write")
+
+        #Initiate write procedure
+        _openedFile.write(line + "\r\n")
