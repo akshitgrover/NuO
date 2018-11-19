9a1e9a37b0c4e0e1b14136d0d2db47f3e45ba6ea
Akshit Grover <akshit.grover2016@gmail.com>
2018-11-20 00:55:04 +0530

    Main: Create build directory before parsing starts

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #! /usr/bin/python
 
+import os
+import shutil
+
 from configParser import parser
 import nuoParser as nps
 import siteData as sd
 
 if __name__ == "__main__":
+    if(os.path.exists("build")):
+        shutil.rmtree("build")
+    os.mkdir("build")
     print("Starting to parse...", end = "\n\n")
     parser.parse("./config.yml")
     sd.siteData()
