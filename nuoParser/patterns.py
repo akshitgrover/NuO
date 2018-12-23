e29a4361991fc318e98e1432fc20c537085e7da1
Akshit Grover <akshit.grover2016@gmail.com>
2018-12-23 14:15:24 +0530

    NuoPatterns: Add arithmetic expression regexp pattern

--- nuoParser/patterns.py
+++ nuoParser/patterns.py
@@ -6,3 +6,6 @@ _rangeEnd = re.compile("{\\s*endRange\\s*}")
 _var = re.compile("{{\\s*[A-Za-z0-9_\-\.]*\\s*}}")
 
 _define = re.compile("{{\\s*define\\s*[A-Za-z0-9_\-]*\\s*[A-Za-z0-9_\-\.]*\\s*}}")
+
+_arithmeticDefine = re.compile("{\\s*define\\s*[a-zA-Z0-9_\-]+\\s*=\\s*((\\s*[a-zA-Z0-9_\-]+\\s*[+*/-]\\s*[a-zA-Z0-9_\-]+\\s*)|(\\s*[+*/-]\\s*[a-zA-Z0-9]+\\s*))*\\s*}")
+_arithmetic = re.compile("{{\\s*((\\s*[a-zA-Z0-9_\-]+\\s*[+*/-]\\s*[a-zA-Z0-9_\-]+\\s*)|(\\s*[+*/-]\\s*[a-zA-Z0-9]+\\s*))*\\s*}}")
