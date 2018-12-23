402cea4855d055bbafbffcc1aa17fbf223b2b279
Akshit Grover <akshit.grover2016@gmail.com>
2018-12-24 00:46:30 +0530

    NuoPatterns: Update arithmetic expression regexp pattern

--- nuoParser/patterns.py
+++ nuoParser/patterns.py
@@ -7,5 +7,5 @@ _var = re.compile("{{\\s*[A-Za-z0-9_\-\.]*\\s*}}")
 
 _define = re.compile("{{\\s*define\\s*[A-Za-z0-9_\-]*\\s*[A-Za-z0-9_\-\.]*\\s*}}")
 
-_arithmeticDefine = re.compile("{\\s*define\\s*[a-zA-Z0-9_\-]+\\s*=\\s*((\\s*[a-zA-Z0-9_\-]+\\s*[+*/-]\\s*[a-zA-Z0-9_\-]+\\s*)|(\\s*[+*/-]\\s*[a-zA-Z0-9]+\\s*))*\\s*}")
-_arithmetic = re.compile("{{\\s*((\\s*[a-zA-Z0-9_\-]+\\s*[+*/-]\\s*[a-zA-Z0-9_\-]+\\s*)|(\\s*[+*/-]\\s*[a-zA-Z0-9]+\\s*))*\\s*}}")
+_arithmeticDefine = re.compile("^{\\s*define\\s+[a-zA-Z0-9_\-]+\\s+=\\s+((\\s+[+*/-]\\s+)?\(?\\s*(\\s+[+*/-]\\s+)?[a-zA-Z0-9_\-]+\\s*\)?)+\\s*}$")
+_arithmetic = re.compile("^{{\\s*((\\s+[+*/-]\\s+)?\(?\\s*(\\s+[+*/-]\\s+)?[a-zA-Z0-9_\-]+\\s*\)?)+\\s*}}$")
